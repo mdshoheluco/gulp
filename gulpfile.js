95724284c0a1155e402b625551fbb97ed3fed0be
Md Shohel <md.shohel@paycomhq.com>
2017-12-13 08:03:13 -0600

    Change in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
 var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var csso = require('gulp-csso');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-    console.log('Hello World!');
+gulp.task('serve', ['styles'], function() {
+  browserSync.init({
+    server: './',
+    port: '8080'
+  });
+});
+
+gulp.task('compress', function() {
+  gulp.src('js/*.js')
+  .pipe(uglify())
+  .on('error', console.error.bind(console))
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('styles', function() {
+  gulp.src('css/*.css')
+  .pipe(csso())
+  .on('error', console.error.bind(console))
+  .pipe(gulp.dest('mincss'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['compress', 'styles', 'watch', 'serve']);
+
+//Gulp watch task
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', ['compress']);
+  gulp.watch('css/*.css', ['styles']);
 });
